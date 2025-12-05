e355e13a46041ac244cfec493d80123316d75e3d
Sowmiya <sowmiyayyanar@gmail.com>
2025-12-05 09:59:43 +0530

    find odd and even numbers
    control statements

---
+++ controlstatements.java
@@ -0,0 +1,24 @@
+package day2;
+
+public class controlstatements {
+
+	public static void main(String[] args) {
+		
+
+		int numb=13;
+		for(int i=0; i<=numb; i++)
+		{
+		if(i%2==0)
+		{
+		System.out.println(i +" yes its odd");
+		}
+		else
+		{
+		System.out.println(i +" no its even");
+		}
+		    
+		}
+		}
+		}
+		    
+
