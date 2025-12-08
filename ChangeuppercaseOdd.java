7218e359a5db0ba14ff000e25fc6d42d7cfaa014
Sowmiya <sowmiyayyanar@gmail.com>
2025-12-09 03:27:56 +0530

    Update ChangeuppercaseOdd.java

--- ChangeuppercaseOdd.java
+++ ChangeuppercaseOdd.java
@@ -6,13 +6,13 @@ public class ChangeuppercaseOdd {
 		String name ="changeMe";
 	    //String rev=name.toUpperCase();
 	    //System.out.println(rev);
-	char[] arr= name.toCharArray();
-	for(int i = 0; i<arr.length; i++) {
-	    if(i%2!=0){
+	char[] arr= name.toCharArray();  //convert to array
+	for(int i = 0; i<arr.length; i++) {  //intro iteration
+	    if(i%2!=0){                      //checking odd place through for loop
 	        
-	        int chn=Character.toUpperCase(i);
-	         System.out.println(chn);
-	    }
+	        int chn=Character.toUpperCase(i);  //change to uppercase with stored in int data type
+	         System.out.println(chn);     //DOUBT** only position diplaying
+	    } 
 	}
 
 	}
