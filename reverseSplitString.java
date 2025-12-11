d5bdce649efd0f97f357fedad0b36688b2c7718f
Sowmiya <sowmiyayyanar@gmail.com>
2025-12-11 19:26:06 +0530

    Update reverseSplitString.java

--- reverseSplitString.java
+++ reverseSplitString.java
@@ -2,13 +2,29 @@ package day3;
 
 public class reverseSplitString {
 
+	public class reverseSplit {
+
 	public static void main(String[] args) {
-		String test = "I am a software tester";
-		   String[] word= test.split(" ");     //splitted using deleimeter
-		   for(int i=0; i>word.length-1; i--){  //iteration happened
-		       System.out.println(word[i]);     //DOUBT not printing 
-		   } 
+		
+		  String text = "We learn Java basics as part of java sessions in java week1";
+		   int  count=0;
+		   String [] divide=text. split(" ");
+		   for(int i =0; i<text.length()-1; i++){
+		       for(int j=0;j<text.length()-1; j++){
+		           if(divide[i].equals(divide[j])){
+		              //divide[j]=" ";
+		               
+		           }
+		            System.out.println("done   = "+ divide );
+		            count++;
+		       }
+		       
+		   }
 		}
+		
+		
+		
+		
 	}
 
 
