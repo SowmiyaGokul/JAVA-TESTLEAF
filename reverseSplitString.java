a9ceaf39f14977d40ed6d1abd89a3152844fd8dc
Sowmiya <sowmiyayyanar@gmail.com>
2025-12-12 10:37:44 +0530

    splitstringodd and even

--- reverseSplitString.java
+++ reverseSplitString.java
@@ -2,31 +2,22 @@ package day3;
 
 public class reverseSplitString {
 
-	public class reverseSplit {
-
-	public class reverseSplit {
-
 	public static void main(String[] args) {
-		
-		  String text = "We learn Java basics as part of java sessions in java week1";
-		  
-		   String [] divide=text. split(" "); //convert string to array
-		   for(int i =0; i<divide.length-1; i++){
-			                                        //intro of 2 loops to compare each and every word
-		       for(int j=0;j<divide.length-1; j++){
-		    	   
-		           if(divide[i].equals(divide[j])){
-		              divide[j]=" ";           //comparing
-		               
-		           }
-		            System.out.println("done   = "+ Arrays.toString(divide) );
-		                                                                        //printing array again to string
-		       }
-		       
-		   }
-		}
-		
-		
-		
-		
-	}
+		 String test = "I am a software tester";
+			String rev="";
+			   String[] word= test.split(" ");     //splitted using deleimeter
+			   for(int i=0; i>word.length; i++){ //iteration happened
+				   if(i%2!=0) {
+				       char[] chnaged = word[i].toCharArray(); }}//convertion happens here
+				       for(int j=0; j>word.length;j++){
+				           if(j%2==0){
+				               System.out.println("printed.  "+ word);}
+				           }
+				       
+				   
+			   }
+}
+				   
+			   
+
+
