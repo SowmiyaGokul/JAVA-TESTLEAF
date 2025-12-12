d3fe10c9584105eca343bf1ec44d786ef35e02ac
Sowmiya <sowmiyayyanar@gmail.com>
2025-12-12 10:04:41 +0530

    Update reverseSplitString.java

--- reverseSplitString.java
+++ reverseSplitString.java
@@ -4,19 +4,23 @@ public class reverseSplitString {
 
 	public class reverseSplit {
 
+	public class reverseSplit {
+
 	public static void main(String[] args) {
 		
 		  String text = "We learn Java basics as part of java sessions in java week1";
-		   int  count=0;
-		   String [] divide=text. split(" ");
-		   for(int i =0; i<text.length()-1; i++){
-		       for(int j=0;j<text.length()-1; j++){
+		  
+		   String [] divide=text. split(" "); //convert string to array
+		   for(int i =0; i<divide.length-1; i++){
+			                                        //intro of 2 loops to compare each and every word
+		       for(int j=0;j<divide.length-1; j++){
+		    	   
 		           if(divide[i].equals(divide[j])){
-		              //divide[j]=" ";
+		              divide[j]=" ";           //comparing
 		               
 		           }
-		            System.out.println("done   = "+ divide );
-		            count++;
+		            System.out.println("done   = "+ Arrays.toString(divide) );
+		                                                                        //printing array again to string
 		       }
 		       
 		   }
@@ -26,5 +30,3 @@ public class reverseSplitString {
 		
 		
 	}
-
-
