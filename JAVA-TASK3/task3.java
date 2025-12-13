e5ad876cba92c9dc0c30e423e9c7b01c2e411ffa
Sowmiya <sowmiyayyanar@gmail.com>
2025-12-13 07:47:13 +0530

    Add method to print indexes of letter 'a'
    Implement functionality to print indexes of 'a' in a string.

--- JAVA-TASK3/task3.java
+++ JAVA-TASK3/task3.java
@@ -22,7 +22,45 @@ public class task3 {
 			
 			
 		}
-		
+
+
+		                        //given task
+		java 3/20 
+
+(Print all indexes of 'a')
+
+Given a string s, print the index of every occurrence of the letter 'a'.
+
+Example:
+
+Input: s = "java programming"
+
+Explanation:
+The string has characters at these positions:
+0 - j
+1 - a
+2 - v
+3 - a
+4 - (space)
+5 - p
+6 - r
+7 - o
+8 - g
+9 - r
+10 - a
+11 - m
+12 - m
+13 - i
+14 - n
+15 - g
+
+The letter 'a' appears on the indexes 1, 3, and 10.
+These are the indexes you must print.
+
+Output:
+1
+3
+	10
 		
 		
 		
